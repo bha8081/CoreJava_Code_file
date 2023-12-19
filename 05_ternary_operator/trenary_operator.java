733fe283f35f7e00cfb4bdeb63f7767643bf60d7
bha8081 <bhaveshy2000r@gmail.com>
2023-12-19 10:22:15 +0530

    Ternary Operator in Java

---
+++ 05_ternary_operator/trenary_operator.java
@@ -0,0 +1,7 @@
+public class trenary_operator{
+    public static void main(String[] args) {
+        int num = 4;
+        String str = (num % 2 == 0)? "Even":"Odd";
+        System.out.println(str);
+    }
+}
