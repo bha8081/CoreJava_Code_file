c985b85ee01ccc1f213363432e2c77f2aeaec44f
bha8081 <bhaveshy2000r@gmail.com>
2023-12-20 10:20:26 +0530

    do while loop

---
+++ 06_loops_in_java/do_while.java
@@ -0,0 +1,10 @@
+public class do_while {
+    public static void main(String[] args) {
+        int x = 7;
+
+        do {
+            System.out.println("Hello Jone " +x);
+            x++;
+        } while(x < 14 && x >= 5);
+    }
+}
