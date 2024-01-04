5671a0eb68d31e45e80dadfde48cfa35e03bc1ec
bha8081 <bhaveshy2000r@gmail.com>
2024-01-04 08:16:29 +0530

    String in Java

--- 09_String/String_Demo.java
+++ 09_String/String_Demo.java
@@ -1,6 +1,8 @@
 public class String_Demo{
     public static void main(String[] args){
-        String name = new String("Shubham");
+        //String name = new String("Shubham");
+
+        String name = "Bhavesh";
 
         System.out.println(name);
         System.out.println(name.hashCode());
