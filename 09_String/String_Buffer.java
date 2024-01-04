87db2c2bc53cd983fd2097bc15177cb8324cba5d
bha8081 <bhaveshy2000r@gmail.com>
2024-01-04 10:07:06 +0530

    StringBuffer in Java

---
+++ 09_String/String_Buffer.java
@@ -0,0 +1,31 @@
+public class String_Buffer{
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer("Bhavesh");
+// Check the StringBuffer Capacity.
+      System.out.println(sb.capacity());
+
+// Check the length.
+      System.out.println(sb.length());
+
+    //  append
+       sb.append(" Yadav");
+       System.out.println(sb);
+
+    // Convert StringBuffer -toString.
+      String str = sb.toString();
+
+    //   Delete method.
+       sb.deleteCharAt(9);
+       System.out.println(sb);
+
+    // Instert data
+       sb.insert(12, " Programmer");
+       System.out.println(sb);
+       
+    // Set the length
+      sb.setLength(29);
+      System.out.println(sb);
+      
+    }
+
+}
