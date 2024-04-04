787f9060bf6d6af0c8aa495b5a42ab6dd070b4a1
bha8081 <bhaveshy2000r@gmail.com>
2024-04-04 09:12:03 +0530

    TreeSet in collection

---
+++ Collection/09_TreeSet/TreeSetDemo.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+public class TreeSetDemo
+{
+    public static void main(String[] args)
+    {
+        TreeSet<Integer> ts = new TreeSet<Integer>();
+
+        ts.add(51);
+        ts.add(87);
+
+        System.out.println(ts);
+    }
+}
